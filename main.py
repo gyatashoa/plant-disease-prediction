b19328404e35b83768eff03029296b4914a15d0b
Asamoah Yeboah Felix <felixasamoah539@gmail.com>
2022-07-29 15:34:01 +0000

    fixed comma error

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 MODEL = tf.keras.models.load_model("./saved_models/1")
 
 
-CLASS_NAMES = ["Early Blight", "Late Blight" "Healthy"]
+CLASS_NAMES = ["Early Blight", "Late Blight" ,"Healthy"]
 
 @app.get("/ping")
 async def ping():
