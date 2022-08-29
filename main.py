ad7087552515b49ff2b5273cc9ee68adf13b1daf
Asamoah Yeboah Felix <felixasamoah539@gmail.com>
2022-08-29 10:59:52 +0000

    added corn and pepper models

--- main.py
+++ main.py
@@ -10,15 +10,14 @@ import tensorflow as tf
 from os import environ
 
 app = FastAPI()
-MODEL = tf.keras.models.load_model("./saved_models/1")
+MODEL_POTATO = tf.keras.models.load_model("./saved_models/1")
+MODEL_CORN = tf.keras.models.load_model("./saved_models/corn/1")
+MODEL_PEPPER = tf.keras.models.load_model("./saved_models/pepper/2")
 
 
-CLASS_NAMES = ["Early Blight", "Late Blight" ,"Healthy"]
-
-@app.get("/ping")
-async def ping():
-    return "rolland"
-
+CLASS_NAMES_POTATO = ["Early Blight", "Late Blight" ,"Healthy"]
+CLASS_NAMES_CORN = ["Corn Blight","Corn Common Rust","Corn Gray Leaf Spot","Healthy"]
+CLASS_NAMES_PEPPER = ["Bell Bacterial Spot","Healthy"]
 
 def read_file_as_image(data) -> np.array:
     image = np.array(Image.open(BytesIO(data )))
@@ -26,7 +25,7 @@ def read_file_as_image(data) -> np.array:
     
     
     
-@app.post("/predict")
+@app.post("/potato")
 async def predict(
     file: UploadFile = File(...)
 ):
@@ -34,9 +33,46 @@ async def predict(
     
     img_batch = np.expand_dims(image, 0)
     
-    predictions = MODEL.predict(img_batch)
+    predictions = MODEL_POTATO.predict(img_batch)
     
-    predicted_class = CLASS_NAMES [np.argmax(predictions[0])]
+    predicted_class = CLASS_NAMES_POTATO [np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    return  {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+    
+    
+    
+@app.post("/corn")
+async def predict(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    
+    img_batch = np.expand_dims(image, 0)
+    
+    predictions = MODEL_CORN.predict(img_batch)
+    
+    predicted_class = CLASS_NAMES_CORN [np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    return  {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+  
+    
+@app.post("/pepper")
+async def predict(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    
+    img_batch = np.expand_dims(image, 0)
+    
+    predictions = MODEL_PEPPER.predict(img_batch)
+    
+    predicted_class = CLASS_NAMES_PEPPER[np.argmax(predictions[0])]
     confidence = np.max(predictions[0])
     return  {
         'class': predicted_class,
